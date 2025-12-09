c195ac0d601376849174751c043a2f255fcf07b9
ManaS0708 <manasbeniwal0708@gmail.com>
2025-12-09 19:56:16 +0530

    wrap in container > adder collor and other decoration > and other visual changes

--- lib/main.dart
+++ lib/main.dart
@@ -10,17 +10,31 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'my_first_app',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: Colors.deepPurple,
+            brightness: Brightness.dark),
+        useMaterial3: true,
+      ),
 
       home: Scaffold(
         body: Center(
 
-          child:
-
-          Text(
-            'Hello world',
-            style: TextStyle(fontSize: 32),
+          child: Container(
+            height: 200.0,
+            width: 200.0,
+            padding: EdgeInsets.all(50.0),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(25.0),
+              color: Colors.red,
+            ),
+            child: Text(
+              'Hello world',
+              style: TextStyle(fontSize: 32),
+            ),
           ),
 
         ),
