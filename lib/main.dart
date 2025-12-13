e6e516ed54691c820965b8df0be7b8b779687f43
ManaS0708 <manasbeniwal0708@gmail.com>
2025-12-13 15:45:24 +0530

    floating action buttons

--- lib/main.dart
+++ lib/main.dart
@@ -28,6 +28,32 @@ class MyApp extends StatelessWidget {
           title: Text("My First App"),
           centerTitle: true,
         ),
+        floatingActionButton: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            FloatingActionButton(
+              onPressed: () {
+
+              },
+              child:
+              Icon(
+                Icons.add
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            FloatingActionButton(
+              onPressed: () {
+
+              },
+              child:
+              Icon(
+                  Icons.add
+              ),
+            ),
+          ],
+        ),
         bottomNavigationBar: NavigationBar(
           destinations: [
             NavigationDestination(
