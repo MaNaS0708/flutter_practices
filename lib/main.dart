da023db4d40b8a285bad3fc01630ca8bc3229e33
ManaS0708 <manasbeniwal0708@gmail.com>
2025-12-13 12:42:26 +0530

    added top app bar simple list tile with onTap()

--- lib/main.dart
+++ lib/main.dart
@@ -15,30 +15,47 @@ class MyApp extends StatelessWidget {
       title: 'my_first_app',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
-            seedColor: Colors.deepPurple,
-            brightness: Brightness.dark),
+          seedColor: Colors.deepPurple,
+          brightness: Brightness.dark,
+        ),
         useMaterial3: true,
       ),
-
       home: Scaffold(
-        body: Stack(
-          children: [
-            Image.asset(
-                'assets/images/wallpaper.jpg',
-                fit: BoxFit.cover,
+        appBar: AppBar(
+          title: const Text('My First App'),
+          backgroundColor:
+          Theme.of(context).colorScheme.inversePrimary,
+          centerTitle: true,
+          leading: Icon(Icons.notifications,
+          color: Colors.red,
+          ),
+        ),
+        body: Container(
+          child: Stack(
+            children: [
+              SizedBox(
                 height: 300,
-            ),
-            SizedBox(
-              height: 300,
-              child: Center(
-                child: Text(
-                  'Hello'
+                child: Center(
+                  child: Text(
+                    'Hello',
+                    style: TextStyle(fontSize: 24),
+                  ),
                 ),
               ),
-            )
-          ],
+              ListTile(
+                leading: Icon(Icons.call, color: Colors.orange,),
+                tileColor: Colors.red,
+                title: Text('skfjbib'),
+                trailing: Text('skjgbug'),
+                onTap: () {
+                  print('clicked on this listtile');
+                },
+              )
+            ],
+          ),
         ),
       ),
     );
   }
+
 }
